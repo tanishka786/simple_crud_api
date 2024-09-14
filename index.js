1dd69e653ba8070892090d9c1605278cfae48d43
TANISHKA JAYESH DAS <147196596+tanishka786@users.noreply.github.com>
2024-09-14 23:00:14 +0530

    connected to database

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 //console.log("Hello there");
 
+//bolier plate code of express
 const express = require('express')
+const mongoose = require('mongoose');
 const app = express()
 
-app.listen(3000, ( ) =>{console.log('server is running on port 3000');});
-
 app.get('/',(req,res)  =>{
-   res.send("Ola,  from node API")
-});
+   res.send("Ola from node API Server ✌🏻"); // coming from node API
+   });
 
+// Connecting to the database   
+mongoose.connect("mongodb+srv://Tan:6VVspGGqVAAEcQCP@backenddb.zukks.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB")
+  .then(() => {
+   console.log('Connected To database!');
+   //running it 
+   app.listen(3000, ( ) =>{
+   console.log('Server is running on port 3000');
+   }); // coming from console screen 
+})
+.catch(() => {
+   console.log('Connection falied!');
+});
